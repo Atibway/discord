9a8943ba60e6e84bb90b3ad3435e7083d2be8fe3
Atibway <ainebyoonaatiidu@gmail.com>
2025-01-15 21:44:48 +0300

    ok

--- app/api/direct-messages/route.ts
+++ app/api/direct-messages/route.ts
@@ -1,81 +1,79 @@
 import { currentProfile } from "@/lib/auth";
 import { db } from "@/lib/db";
-import { DirectMessage} from "@prisma/client";
+import { DirectMessage } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 const MESSAGES_BATCH = 10;
 
-export async function GET(
-    req: Request
-){
-try {
+export async function GET(req: Request) {
+  try {
     const profile = await currentProfile();
-    const {searchParams} = new URL(req.url)
+    const { searchParams } = new URL(req.url);
 
-    const cursor = searchParams.get("cursor")
-    const conversationId = searchParams.get("conversationId")
+    const cursor = searchParams.get("cursor");
+    const conversationId = searchParams.get("conversationId");
 
     if (!profile) {
-        return new NextResponse("unauthorized", { status: 401 });
+      return new NextResponse("unauthorized", { status: 401 });
     }
     if (!conversationId) {
-        return new NextResponse("Channel Id Missing", { status: 400 });
+      return new NextResponse("Channel Id Missing", { status: 400 });
     }
-    
-    let messages: DirectMessage[] = []
 
-    if(cursor){
-        messages = await db.directMessage.findMany({
-            take: MESSAGES_BATCH,
-            skip: 1,
-            cursor: {
-                id: cursor,
+    let messages: DirectMessage[] = [];
+
+    if (cursor) {
+      messages = await db.directMessage.findMany({
+        take: MESSAGES_BATCH,
+        skip: 1,
+        cursor: {
+          id: cursor,
+        },
+        where: {
+          conversationId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
             },
-            where:{
-                conversationId
-            },
-            include:{
-                member: {
-                    include: {
-                        profile: true
-                    }
-                }
-            },
-            orderBy: {
-                createdAt: "desc"
-            }
-        })
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
     } else {
-        messages = await db.directMessage.findMany({
-            take: MESSAGES_BATCH,
-            where:{
-                conversationId
+      messages = await db.directMessage.findMany({
+        take: MESSAGES_BATCH,
+        where: {
+          conversationId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
             },
-            include:{
-                member: {
-                    include: {
-                        profile: true
-                    }
-                }
-            },
-            orderBy: {
-                createdAt: "desc"
-            }
-        })
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
     }
-    
-    let nextCursor = null;
 
-    if(messages.length === MESSAGES_BATCH){
-nextCursor = messages[MESSAGES_BATCH - 1].id
+    let nextCursor: string | null = null;
+
+    if (messages.length === MESSAGES_BATCH) {
+      nextCursor = messages[MESSAGES_BATCH - 1]?.id || null;
     }
 
     return NextResponse.json({
-        items: messages,
-        nextCursor
+      items: messages,
+      nextCursor,
     });
-} catch (error) {
-    console.log(["DIRECT_MESSAGR_GET", error]);
+  } catch (error) {
+    console.log(["DIRECT_MESSAGE_GET", error]);
     return new NextResponse("internal server error", { status: 500 });
-}
+  }
 }
