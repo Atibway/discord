93a33f5e25f7489a4cdb6cdd931c50b7eb285d45
Atibway <ainebyoonaatiidu@gmail.com>
2025-01-15 21:45:57 +0300

    ok

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -5,77 +5,75 @@ import { NextResponse } from "next/server";
 
 const MESSAGES_BATCH = 10;
 
-export async function GET(
-    req: Request
-){
-try {
+export async function GET(req: Request) {
+  try {
     const profile = await currentProfile();
-    const {searchParams} = new URL(req.url)
+    const { searchParams } = new URL(req.url);
 
-    const cursor = searchParams.get("cursor")
-    const channelId = searchParams.get("channelId")
+    const cursor = searchParams.get("cursor");
+    const channelId = searchParams.get("channelId");
 
     if (!profile) {
-        return new NextResponse("unauthorized", { status: 401 });
+      return new NextResponse("unauthorized", { status: 401 });
     }
     if (!channelId) {
-        return new NextResponse("Channel Id Missing", { status: 400 });
+      return new NextResponse("Channel Id Missing", { status: 400 });
     }
-    
-    let messages: Message[] = []
 
-    if(cursor){
-        messages = await db.message.findMany({
-            take: MESSAGES_BATCH,
-            skip: 1,
-            cursor: {
-                id: cursor,
+    let messages: Message[] = [];
+
+    if (cursor) {
+      messages = await db.message.findMany({
+        take: MESSAGES_BATCH,
+        skip: 1,
+        cursor: {
+          id: cursor,
+        },
+        where: {
+          channelId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
             },
-            where:{
-                channelId
-            },
-            include:{
-                member: {
-                    include: {
-                        profile: true
-                    }
-                }
-            },
-            orderBy: {
-                createdAt: "desc"
-            }
-        })
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
     } else {
-        messages = await db.message.findMany({
-            take: MESSAGES_BATCH,
-            where:{
-                channelId
+      messages = await db.message.findMany({
+        take: MESSAGES_BATCH,
+        where: {
+          channelId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
             },
-            include:{
-                member: {
-                    include: {
-                        profile: true
-                    }
-                }
-            },
-            orderBy: {
-                createdAt: "desc"
-            }
-        })
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
     }
-    
-    let nextCursor = null;
 
-    if(messages.length === MESSAGES_BATCH){
-nextCursor = messages[MESSAGES_BATCH - 1].id
+    let nextCursor: string | null = null;
+
+    if (messages.length === MESSAGES_BATCH) {
+      nextCursor = messages[MESSAGES_BATCH - 1]?.id || null;
     }
 
     return NextResponse.json({
-        items: messages,
-        nextCursor
+      items: messages,
+      nextCursor,
     });
-} catch (error) {
-    console.log(["MESSAGR_GET", error]);
+  } catch (error) {
+    console.log(["MESSAGE_GET", error]);
     return new NextResponse("internal server error", { status: 500 });
-}
+  }
 }
